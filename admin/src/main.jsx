83df83caa3fb96d1b0686972a920479bd8bfa724
Clivon Osire <clivon254@icloud.com>
2024-08-21 21:09:12 +0300

    redux

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from "react-redux"
 import App from './App.jsx'
 import './index.css'
+import { persistor, store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+import ThemeProvider from './components/ThemeProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    
+    <Provider  store={store}>
+
+      <PersistGate loading={null} persistor={persistor}>
+
+        <ThemeProvider>
+
+          <App />
+        
+         </ThemeProvider>
+      
+      </PersistGate>
+
+    </Provider>
+
   </StrictMode>,
 )
---
+++ admin/src/redux/store.js
@@ -0,0 +1,31 @@
+
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist"
+import userReducer from "./user/userSlice";
+import themeReducer from "./theme/themeSlice";
+import storage from "redux-persist/lib/storage"
+
+
+const rootReducer = combineReducers({
+    user:userReducer,
+    theme:themeReducer
+})
+
+
+const persistConfig = {
+    key:'root',
+    storage,
+    version:1
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+
+    reducer:persistedReducer,
+
+    middleware:(getDefaultMiddleware) => 
+        getDefaultMiddleware({serializableCheck:false})
+})
+
+export const persistor = persistStore(store)
